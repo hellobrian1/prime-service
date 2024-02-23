8f145f55bc058ad1028d7fdb3281fc198fd20ff8
LukeCali-949 <mengluke17@gmail.com>
2024-02-22 19:13:19 -0500

    commit v3

--- src/main/java/edu/iu/lukemeng/primesservice/controller/HomeController.java
+++ src/main/java/edu/iu/lukemeng/primesservice/controller/HomeController.java
@@ -3,7 +3,7 @@ package edu.iu.lukemeng.primesservice.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//edit
 @RestController
 public class HomeController implements ErrorController {
     @GetMapping("/")
